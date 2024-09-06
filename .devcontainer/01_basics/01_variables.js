062245671dd278737504cce0fa358758acdf5f9c
prince kumar <166170708+pk-pythondeveloper@users.noreply.github.com>
2024-09-06 15:38:54 +0000

    these are variables

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,14 @@
+var a="ram";
+a=34;
+//var is able to redefine,reshine and redicleared
+// it can be declared and accesses globally
+
+let b=34;
+//let can be decleared globally but its access is limited to the block in which it is declared.
+//its not be re-decleared within the same scope
+
+const ram=23;
+//can not ressigned value can not be changedonce its has been decleared
+console.log("these are the variables in java script")
+
+
