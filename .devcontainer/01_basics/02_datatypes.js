864f7647918544993a05bbde61e775149148d506
prince kumar <166170708+pk-pythondeveloper@users.noreply.github.com>
2024-09-06 15:53:33 +0000

    changed

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+//number:-its storte number
+let a=1233
+//bigint:-it use when we have the number larger the 2 to the power 53
+
+// string :-it use to store the crater or group of chracter
+let a="tree"
+
+//boolean;-it store true and false
+let password=True
+
+//null:-its mean value should be empty
+let 
+
