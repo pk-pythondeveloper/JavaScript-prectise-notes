1158081d8a14ce34deb7f4f0862dd405feba6411
prince kumar <166170708+pk-pythondeveloper@users.noreply.github.com>
2024-09-08 21:24:53 +0000

    use type of operator

--- .devcontainer/01_basics/02_datatypes.js
+++ .devcontainer/01_basics/02_datatypes.js
@@ -3,11 +3,29 @@ let a=1233
 //bigint:-it use when we have the number larger the 2 to the power 53
 
 // string :-it use to store the crater or group of chracter
-let a="tree"
+let apana="tree"
 
 //boolean;-it store true and false
-let password=True
+//let password=True
 
 //null:-its mean value should be empty
-let 
+//let age=Null
 
+//Type of operator
+let stu_name="prince_kumar"
+console.log(typeof(stu_name))
+
+let num=123455
+console.log(typeof(num))
+
+num_value=String(num)
+console.log(typeof(num_value))
+console.log(num_value)
+
+//let we have a string we ant change into the nember
+let str="prince123"
+
+let str1=Number(str)
+console.log(typeof(str1))
+console.log(str1)
+//when we want to change a variables to another variables but its can not able to change its give NAN
