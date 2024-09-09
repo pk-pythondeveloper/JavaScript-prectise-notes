19df188364db09d753d24b60fa38f0d8b3ad6c7d
prince kumar <166170708+pk-pythondeveloper@users.noreply.github.com>
2024-09-09 10:32:04 +0000

    postfix prefix

---
+++ .devcontainer/01_basics/03_operations.js
@@ -0,0 +1,14 @@
+//********operations **** */
+
+console.log(2+2) //we get 4
+console.log("2"+2) //we get 22 beacause first value is string
+console.log("2"+2+4) //here we also get 224 due to its first value is string
+
+console.log(2+6+"5") //we get 85 due to its first  value is Number
+console.log("2"+"2") //we get 22
+
+let num=1000
+++num;//it increase the vlaue by one its also called prefix
+console.log(num)
+num++;//in postfix also increase the value by 1
+console.log(num)
