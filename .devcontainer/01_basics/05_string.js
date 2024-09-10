3b3e0e1199968905d565f9e033c277b5404b0a5d
prince kumar <166170708+pk-pythondeveloper@users.noreply.github.com>
2024-09-10 10:13:36 +0000

    sting functions

---
+++ .devcontainer/01_basics/05_string.js
@@ -0,0 +1,16 @@
+// const name="ramanujam";
+// console.log(typeof(name))
+// console.log(name[2])
+
+const newstr=new String("paritosh shahu") //new keyword is used to create an instance of an object that has a constructor function
+// console.log(newstr)
+// console.log(typeof(newstr))
+// console.log(newstr[2])
+
+// //+++++++++++++++++string methods++++++++++++++++ 
+// console.log(newstr.length)//to find the length of string
+// console.log(newstr.toUpperCase())
+console.log(newstr.charAt('p'));
+
+
+
