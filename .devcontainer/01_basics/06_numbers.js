a2319175c4a056437c3665782d7ad6fa276c0559
prince kumar <166170708+pk-pythondeveloper@users.noreply.github.com>
2024-09-10 20:29:25 +0000

    method s

---
+++ .devcontainer/01_basics/06_numbers.js
@@ -0,0 +1,23 @@
+//++++++++++++++++Numbers++++++++++++
+const num=50000;
+console.log(typeof(num))
+console.log(num.toString().length)//the change the number he string
+const num1=35332.0809234
+console.log(num1.toFixed(2))//it fixed the after decimal place
+const num2=4000000000000
+console.log(num2.toLocaleString('en-IN'))//change the number in countable indian form
+
+//++++++++++math++++++++
+console.log(Math.abs(-4))//change neagative value  to opsitive 
+console.log(Math.abs(4))//change neagative value  to opsite and opsitive just like that
+console.log(Math.round(56.9))//it geive the round of value according to digit after decimal
+console.log(Math.ceil(5.2))//it takes next value if value .1 percent grater the given value like 6
+console.log(Math.floor(4.4))//its remove the value after the decimal
+console.log(Math.pow(2,3))//it use to find the Square
+console.log(Math.min(-4783478,7,8,734,646,62378))//to get min value
+console.log(Math.max(-4783478,7,8,734,646,62378))//to get min value
+console.log(Math.rendom(-4783478,7,8,734,646,62378))//to gives the rendom value
+
+
+
+
