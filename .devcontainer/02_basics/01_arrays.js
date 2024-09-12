845a9575fdf13ebe9cf25e0bacd20f277d45941d
prince kumar <166170708+pk-pythondeveloper@users.noreply.github.com>
2024-09-12 11:13:43 +0000

    arrays methids

--- .devcontainer/02_basics/01_arrays.js
+++ .devcontainer/02_basics/01_arrays.js
@@ -2,4 +2,15 @@
 //array is collection of diffrent types of elements or same timps its and elements
 //array is also mutable changeable 
 const myarr=[0,2,4,5,6,78,52,"prince"]
+const arr3=new Array('ram','nam','hai','mera')//array declearation by using contrctore
 console.log(myarr[3])
+
+
+//++++++++++methoids++++++
+const arr=[3,4,6,3,8,3,5]
+arr.push(8)//push the element end of array
+arr.pop()//remove the last elements
+arr.unshift(11)//add the element starting of array
+arr.shift()//remove frist elemnt
+
+console.log(arr.includes(9))//we get value in boolean
