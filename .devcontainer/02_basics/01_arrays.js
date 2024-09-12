cd921b27ff6a3a1f8b68bf296c377bca99bde43b
prince kumar <166170708+pk-pythondeveloper@users.noreply.github.com>
2024-09-12 09:19:51 +0000

    array

---
+++ .devcontainer/02_basics/01_arrays.js
@@ -0,0 +1,5 @@
+//++++++++++Aray+++++++
+//array is collection of diffrent types of elements or same timps its and elements
+//array is also mutable changeable 
+const myarr=[0,2,4,5,6,78,52,"prince"]
+console.log(myarr[3])
